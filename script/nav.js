7f7eee6f27eb29a3dd364c7878f856d3515be77b
minidevz <muhammadibrahimhisham@gmail.com>
2021-01-11 20:22:04 +0500

    removed

--- script/nav.js
+++
@@ -1,8 +0,0 @@
-function mobileToggle() {
-    var x = document.getElementById("topNavbar");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
